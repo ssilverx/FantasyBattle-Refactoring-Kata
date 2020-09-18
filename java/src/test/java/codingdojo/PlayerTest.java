ae638f32118cf8654172300a394754ba4271abe3
Silver <silver.samarutel@fortumo.com>
2020-09-18 10:04:14 +0300

    add PlayerTest.java and pom.xml

--- java/src/test/java/codingdojo/PlayerTest.java
+++ java/src/test/java/codingdojo/PlayerTest.java
@@ -4,20 +4,32 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public class PlayerTest {
 
-    // choose this one if you are familiar with mocks
-    @Disabled("Test is not finished yet")
     @Test
     void damageCalculationsWithMocks() {
         Inventory inventory = mock(Inventory.class);
+        final Equipment equipment = mock(Equipment.class);
+        final Item item = mock(Item.class);
+        given(item.getBaseDamage()).willReturn(10);
+        given(item.getDamageModifier()).willReturn(1.0f);
+        given(equipment.getLeftHand()).willReturn(item);
+        given(equipment.getRightHand()).willReturn(item);
+        given(equipment.getChest()).willReturn(item);
+        given(equipment.getFeet()).willReturn(item);
+        given(equipment.getHead()).willReturn(item);
+
+        given(inventory.getEquipment()).willReturn(equipment);
+
         Stats stats = mock(Stats.class);
         SimpleEnemy target = mock(SimpleEnemy.class);
+        given(target.getArmor()).willReturn(mock(Armor.class));
 
         Damage damage = new Player(inventory, stats).calculateDamage(target);
-        assertEquals(10, damage.getAmount());
+        assertEquals(250, damage.getAmount());
     }
 
     // choose this one if you are not familiar with mocks
